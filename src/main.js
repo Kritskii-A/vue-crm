24b53a4ee023082bb66e8700df97a5b0bd2430f2
Kritskii-A <kritskii-a@bk.ru>
2020-11-12 14:30:30 +0300

    v 0.1

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuelidate from "vuelidate"; // импортируем валидатор
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import dateFilter from "@/filters/date.filter"; // импортируем фильтр
+import "materialize-css/dist/js/materialize.min.js"; // импортируем стили
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuelidate); // регистрируем валидатор после импорта
+Vue.filter("date", dateFilter); // регистрируем фильтр
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");

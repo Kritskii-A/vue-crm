3b0d7ce6c16cb7046356bf6ce03c08ea0ab0bd54
Kritskii-A <kritskii-a@bk.ru>
2020-11-30 16:15:01 +0300

    output planning

--- src/store/record.js
+++ src/store/record.js
@@ -14,5 +14,26 @@ export default {
         throw e;
       }
     },
+    async fetchRecords({ dispatch, commit }) {
+      try {
+        const uid = await dispatch("getUid"); // ждем и получаем uid пользователя
+        const records =
+          (
+            await firebase
+              .database()
+              .ref(`/users/${uid}/records`)
+              .once("value")
+          ) //получаем значения
+            .val() || {}; // получаем значения, и если вдруг ничего нет, то пвозвращаем пустой объект
+
+        return Object.keys(records).map((key) => ({
+          ...records[key],
+          id: key,
+        }));
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
+    },
   },
 };
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,19 +2,76 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ info.bill | currency("RUB") }}</h4>
     </div>
 
-    <section>
-      <div>
+    <loader v-if="loading" />
+
+    <p class="center" v-else-if="!categories.length">
+      Категории не найдены.
+      <router-link to="/categories">Добавить новую категорию</router-link>
+    </p>
+
+    <section v-else>
+      <div v-for="cat of categories" :key="cat.id">
         <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
+          <strong>{{ cat.title }}:</strong>
+          {{ cat.spend | currency }} из {{ cat.limit | currency }}
         </p>
         <div class="progress">
-          <div class="determinate green" style="width:40%"></div>
+          <div
+            class="determinate"
+            :class="[cat.progressColor]"
+            :style="{ width: cat.progressPercent + '%' }"
+          ></div>
         </div>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex"; // для вывода состояние счета
+import Loader from "../components/app/Loader.vue";
+export default {
+  components: { Loader },
+  name: "planning",
+  data: () => ({
+    loading: true,
+    categories: [],
+  }),
+  computed: {
+    ...mapGetters(["info"]), // для вывода состояния счета
+  },
+  async mounted() {
+    const records = await this.$store.dispatch("fetchRecords"); // получаем записи
+    const categories = await this.$store.dispatch("fetchCategories"); // получаем категории
+
+    this.categories = categories.map((cat) => {
+      const spend = records
+        .filter((r) => r.categoryId === cat.id) // ищем те, у которых совпадают id
+        .filter((r) => r.type === "outcome") // получаем все траты
+        .reduce((total, record) => {
+          return (total += +record.amount); // считаем сумму всех трат
+        }, 0);
+
+      const percent = (100 * spend) / cat.limit; // считаем процент
+      const progressPercent = percent > 100 ? 100 : percent; // проверка, чтобы процент не был больше 100
+      const progressColor =
+        percent < 60 ? "green" : percent < 100 ? "yellow" : "red"; // задаем цвет в зависимости от процентов
+
+      return {
+        ...cat,
+        progressPercent,
+        progressColor,
+        spend, // сколько итого потрачено
+      };
+    });
+
+    console.log(records);
+    console.log(categories);
+
+    this.loading = false;
+  },
+};
+</script>

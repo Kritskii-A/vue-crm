55b879956336f0ad5a0e74da558f198df2d54078
Kritskii-A <kritskii-a@bk.ru>
2020-12-02 16:45:34 +0300

    uodate profile name

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,11 +4,31 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @click.prevent="submitHandler">
       <div class="input-field">
-        <input id="description" type="text" />
+        <input
+          id="description"
+          type="text"
+          v-model="name"
+          :class="{
+            invalid: $v.name.$dirty && !$v.name.required,
+          }"
+        />
         <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
+        <small
+          class="helper-text invalid"
+          v-if="$v.name.$dirty && !$v.name.required"
+          >Введите имя</small
+        >
+      </div>
+
+      <div class="switch">
+        <label>
+          English
+          <input type="checkbox" />
+          <span class="lever"></span>
+          Русский
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -18,3 +38,50 @@
     </form>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from "vuex"; // mapGetters - если будем получать данные, mapActions - если будем отправлять данные
+import { required } from "vuelidate/lib/validators"; // импортируем валидаторы, которые будем использовать
+export default {
+  data: () => ({
+    name: "",
+  }),
+  validations: {
+    name: { required },
+  },
+  mounted() {
+    this.name = this.info.name; // вставляем имя
+    // таймаут нужен, чтобы успела отрендериться страница
+    setTimeout(() => {
+      window.M.updateTextFields();
+    });
+  },
+  methods: {
+    ...mapActions(["updateInfo"]),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+        });
+        this.$message("Данные обновлены");
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  computed: {
+    ...mapGetters(["info"]),
+  },
+};
+</script>
+
+<style scoped>
+.switch {
+  margin-bottom: 2rem;
+}
+</style>
